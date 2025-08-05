bfb532e0d633a24fa9906d0b0a86ce763ef0c865
wty2333 <486116915@qq.com>
2025-08-05 14:10:01 +0800

    gou config

---
+++ PipeServe/gpu_config.py
@@ -0,0 +1,26 @@
+
+
+
+
+class GPU:
+
+    def __init__(self, name, flops_efficiency, hbm_memory_efficiency,
+                 mem_per_GPU_in_GB):
+        self.name = name
+        self.flops_efficiency = flops_efficiency
+        self.hbm_memory_efficiency = hbm_memory_efficiency
+        self.mem_per_GPU_in_GB = mem_per_GPU_in_GB
+
+
+def load_gpus_from_config(gpu_name):
+    with open(f"{Path(__file__).parent}/{GPU_CONFIG_FILE}", 'r') as file:
+        data = json.load(file)
+        # gpus = []
+        for name, attributes in data.items():
+            if name == gpu_name:
+                gpu = GPU(
+                    name,
+                    flops_efficiency=attributes['flops_efficiency'],
+                    hbm_memory_efficiency=attributes['hbm_memory_efficiency'],
+                    mem_per_GPU_in_GB=attributes['mem_per_GPU_in_GB'])
+        return gpu
